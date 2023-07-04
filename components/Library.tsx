ac0e44c6861dde0f638293232dca3b76fbb68b42
Ibrahim Dogruer <ibrahim.dogruer.97@gmail.com>
2023-07-04 18:31:49 +0300

    upload songs and images, list of songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,28 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
   const onClick = () => {
-    // Handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    //TODO: Check for subscription
+
+    return uploadModal.onOpen();
   };
 
   return (
@@ -21,7 +40,9 @@ const Library = () => {
       </div>
 
       <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of Songs!
+        {songs.map((item) => (
+          <MediaItem key={item.id} onClick={() => {}} data={item} />
+        ))}
       </div>
     </div>
   );
